b0e9d0d9b9aa382a3991f52d6123eb09bb173bc6
nlwkobe30 <nlwkobe30@gmail.com>
2021-11-26 00:06:36 +0800

    format

--- Binary-Search/167.Two-Sum-II-Input-array-is-sorted/index.ts
+++ Binary-Search/167.Two-Sum-II-Input-array-is-sorted/index.ts
@@ -5,36 +5,37 @@
 // time: O(n)
 // space: O(n)
 function twoSum_map(numbers: number[], target: number): number[] {
-  const map = new Map<number, number>();
+  const map = new Map<number, number>()
 
   for (let i = 0; i < numbers.length; i++) {
-    const theOtherNumber = target - numbers[i];
+    const theOtherNumber = target - numbers[i]
 
     if (!map.has(theOtherNumber)) {
-      map.set(numbers[i], i + 1);
+      map.set(numbers[i], i + 1)
     } else {
-      return [map.get(theOtherNumber)!, i + 1];
+      return [map.get(theOtherNumber)!, i + 1]
     }
   }
-  throw new Error("");
+  throw new Error('')
 }
 
 // time: O(n)
 // space: O(1)
 function twoSum_twoPointers(numbers: number[], target: number): number[] {
-  let left = 0;
-  let right = numbers.length - 1;
+  let left = 0
+  let right = numbers.length - 1
 
   while (left < right) {
     if (numbers[left] + numbers[right] === target) {
-      return [left + 1, right + 1];
+      return [left + 1, right + 1]
     }
     if (numbers[left] + numbers[right] > target) {
-      right--;
+      right--
     }
     if (numbers[left] + numbers[right] < target) {
-      left++;
+      left++
     }
   }
-  throw new Error("");
+  throw new Error('')
+}
 }
