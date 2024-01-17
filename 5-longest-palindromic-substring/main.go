f85f800f27e4d3f3065c2f66c46ba8b2a2ac3266
nlwkobe30 <nlwkobe30@gmail.com>
2024-01-17 23:12:00 +0800

    update 5

---
+++ 5-longest-palindromic-substring/main.go
@@ -0,0 +1,50 @@
+package main
+
+func main() {
+}
+
+// time: O(n^2)
+// space: O(1)
+func longestPalindrome(s string) string {
+	result := s[:1]
+
+	// use i as center, move left & right pointer
+	for i := 1; i < len(s)-1; i++ {
+		left := i - 1
+		right := i + 1
+
+		for left >= 0 && right <= len(s)-1 {
+			if s[left] != s[right] {
+				break
+			}
+
+			if right-left+1 > len(result) {
+				result = s[left : right+1]
+			}
+
+			left--
+			right++
+		}
+	}
+
+	// use adjacent as start point, move left & right pointer
+	for i := 0; i < len(s)-1; i++ {
+		left := i
+		right := i + 1
+
+		for left >= 0 && right <= len(s)-1 {
+			if s[left] != s[right] {
+				break
+			}
+
+			if right-left+1 > len(result) {
+				result = s[left : right+1]
+			}
+
+			left--
+			right++
+		}
+	}
+
+	return result
+}
