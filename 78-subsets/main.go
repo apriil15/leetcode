a150dffed92d712913c3cf333b7086b76e57815f
nlwkobe30 <nlwkobe30@gmail.com>
2024-02-02 22:06:06 +0800

    update

--- 78-subsets/main.go
+++ 78-subsets/main.go
@@ -29,3 +29,34 @@ func subsets(nums []int) [][]int {
 	recursion(0, []int{})
 	return result
 }
+
+//                    *
+//              /           \         add 1 or not
+//             1             []
+//        /       \       /      \    add 2 or not
+//     1,2         1     2       []
+//    /   \      /  \   /  \    / \   add 3 or not
+// 1,2,3  1,2  1,3  1  2,3  2  3  []
+func subsets_dfs(nums []int) [][]int {
+	result := [][]int{}
+
+	var dfs func(i int, sub []int)
+	dfs = func(i int, sub []int) {
+		if i == len(nums) {
+			dest := make([]int, 0, len(sub))
+			dest = append(dest, sub...)
+
+			result = append(result, dest)
+			return
+		}
+
+		sub = append(sub, nums[i])
+		dfs(i+1, sub)
+
+		sub = sub[:len(sub)-1]
+		dfs(i+1, sub)
+	}
+
+	dfs(0, []int{})
+	return result
+}
