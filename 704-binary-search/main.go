748e2dcda31806afec9b2de5c46424d1ed2a322c
nlwkobe30 <nlwkobe30@gmail.com>
2024-02-21 21:51:02 +0800

    update

---
+++ 704-binary-search/main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+}
+
+// time: O(logn)
+// space: O(1)
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return -1
+}
