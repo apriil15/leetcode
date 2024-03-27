66b1997fcc1ad82d034022cc03baf9284c2d20ad
nlwkobe30 <nlwkobe30@gmail.com>
2024-03-27 22:12:15 +0800

    update dfs stack solution

--- 0104-maximum-depth-of-binary-tree/main.go
+++ 0104-maximum-depth-of-binary-tree/main.go
@@ -1,10 +1,18 @@
 package main
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+type (
+	TreeNode struct {
+		Val   int
+		Left  *TreeNode
+		Right *TreeNode
+	}
+
+	// Node for maxDepth_dfs_stack
+	Node struct {
+		node  *TreeNode
+		level int
+	}
+)
 
 func main() {
 
@@ -22,6 +30,47 @@ func maxDepth(root *TreeNode) int {
 	right := maxDepth(root.Right)
 
 	return max(left, right) + 1
+}
+
+// DFS (stack)
+// time: O(n)
+// space: O(n)
+func maxDepth_dfs_stack(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	stack := []Node{
+		{
+			node:  root,
+			level: 1,
+		},
+	}
+
+	result := 1
+	for len(stack) > 0 {
+		// pop
+		last := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		result = max(result, last.level)
+
+		if last.node.Left != nil {
+			stack = append(stack, Node{
+				node:  last.node.Left,
+				level: last.level + 1,
+			})
+		}
+
+		if last.node.Right != nil {
+			stack = append(stack, Node{
+				node:  last.node.Right,
+				level: last.level + 1,
+			})
+		}
+	}
+
+	return result
 }
 
 func max(a, b int) int {
