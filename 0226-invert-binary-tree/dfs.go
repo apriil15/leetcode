d6d469b3a1f8cd8a81cabd3dd5ccb78fc2caed7f
nlwkobe30 <nlwkobe30@gmail.com>
2024-03-29 21:37:22 +0800

    update dfs stack solution

---
+++ 0226-invert-binary-tree/dfs.go
@@ -0,0 +1,31 @@
+package main
+
+// DFS (stack)
+// time: O(n)
+// space: O(n)
+func invertTree_dfs(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	stack := []*TreeNode{root}
+
+	for len(stack) > 0 {
+		last := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		// swap
+		tmp := last.Left
+		last.Left = last.Right
+		last.Right = tmp
+
+		if last.Left != nil {
+			stack = append(stack, last.Left)
+		}
+		if last.Right != nil {
+			stack = append(stack, last.Right)
+		}
+	}
+
+	return root
+}
