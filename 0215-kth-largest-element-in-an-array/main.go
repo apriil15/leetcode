b1b6346a77be76978dc70837374ce80743b5e1d5
nlwkobe30 <nlwkobe30@gmail.com>
2024-06-28 22:22:15 +0800

    update 0215 minHeap with k count

---
+++ 0215-kth-largest-element-in-an-array/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"cmp"
+
+	"github.com/emirpasic/gods/queues/priorityqueue"
+)
+
+func main() {
+
+}
+
+// minHeap with k count
+// time: O((n log n)
+// space: O(k)
+func findKthLargest(nums []int, k int) int {
+	minHeap := priorityqueue.NewWith(func(a, b any) int {
+		return cmp.Compare(a.(int), b.(int))
+	})
+	for _, v := range nums {
+		minHeap.Enqueue(v)
+		if minHeap.Size() > k {
+			minHeap.Dequeue()
+		}
+	}
+
+	res, _ := minHeap.Peek()
+	return res.(int)
+}
