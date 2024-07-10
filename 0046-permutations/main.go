846bbedc5b039d1713826d21509857e70f4f25e2
nlwkobe30 <nlwkobe30@gmail.com>
2024-07-11 00:09:51 +0800

    update 0046 backtracking

--- 0046-permutations/main.go
+++ 0046-permutations/main.go
@@ -12,34 +12,36 @@ func main() {
 // |  |  |  |  | |
 // 4  3  4  2  3 2
 
-// time complexity: O(n*n!)
+// time: O(n * n!)
 func permute(nums []int) [][]int {
-	result := [][]int{}
+	var res [][]int
+	var per []int
+	seen := make(map[int]struct{})
 
-	var backtrack func(m map[int]struct{}, arr []int)
-	backtrack = func(m map[int]struct{}, arr []int) {
-		if len(arr) == len(nums) {
-			dest := make([]int, 0, len(arr))
-			dest = append(dest, arr...)
-
-			result = append(result, dest)
+	var backtrack func()
+	backtrack = func() {
+		if len(per) == len(nums) {
+			dest := make([]int, len(per))
+			copy(dest, per)
+			res = append(res, dest)
 			return
 		}
 
-		for i, num := range nums {
-			if _, ok := m[i]; ok {
+		for i := 0; i < len(nums); i++ {
+			if _, ok := seen[i]; ok {
 				continue
 			}
-			m[i] = struct{}{}
 
-			arr = append(arr, num)
-			backtrack(m, arr)
+			per = append(per, nums[i])
+			seen[i] = struct{}{}
 
-			delete(m, i)
-			arr = arr[:len(arr)-1]
+			backtrack()
+
+			per = per[:len(per)-1]
+			delete(seen, i)
 		}
 	}
 
-	backtrack(map[int]struct{}{}, []int{})
-	return result
+	backtrack()
+	return res
 }
