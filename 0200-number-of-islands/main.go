88b42277483d6ecac2eb783493782391aa755a1f
nlwkobe30 <nlwkobe30@gmail.com>
2024-07-29 22:43:07 +0800

    update 0200 dfs

---
+++ 0200-number-of-islands/main.go
@@ -0,0 +1,41 @@
+package main
+
+func main() {
+
+}
+
+func numIslands(grid [][]byte) int {
+	rows := len(grid)
+	columns := len(grid[0])
+
+	var dfs func(i, j int)
+	dfs = func(i, j int) {
+		if i < 0 || j < 0 || i >= rows || j >= columns {
+			return
+		}
+		if grid[i][j] == '0' {
+			return
+		}
+
+		// if it is '1' (land), then set '0' as walked
+		grid[i][j] = '0'
+
+		// up, down, left, right
+		dfs(i-1, j)
+		dfs(i+1, j)
+		dfs(i, j-1)
+		dfs(i, j+1)
+	}
+
+	var res int
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			if grid[i][j] == '1' {
+				res++
+				dfs(i, j)
+			}
+		}
+	}
+
+	return res
+}
