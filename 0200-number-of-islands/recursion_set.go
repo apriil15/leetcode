053dd4e91517302ae18b67521376a258390e01b8
nlwkobe30 <nlwkobe30@gmail.com>
2024-07-29 23:32:37 +0800

    update 0200 recursion with set

---
+++ 0200-number-of-islands/recursion_set.go
@@ -0,0 +1,47 @@
+package main
+
+func numIslands_recursion_with_set(grid [][]byte) int {
+	rows := len(grid)
+	columns := len(grid[0])
+	visit := make(map[point]struct{})
+
+	var dfs func(i, j int)
+	dfs = func(i, j int) {
+		if i < 0 || j < 0 || i >= rows || j >= columns {
+			return
+		}
+		if _, ok := visit[point{i, j}]; ok {
+			return
+		}
+		if grid[i][j] == '0' {
+			return
+		}
+
+		visit[point{i, j}] = struct{}{}
+
+		// up, down, left, right
+		dfs(i-1, j)
+		dfs(i+1, j)
+		dfs(i, j-1)
+		dfs(i, j+1)
+	}
+
+	var res int
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			if _, ok := visit[point{i, j}]; !ok {
+				if grid[i][j] == '1' {
+					res++
+					dfs(i, j)
+				}
+			}
+		}
+	}
+
+	return res
+}
+
+type point struct {
+	row int
+	col int
+}
