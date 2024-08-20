d5b1e7bdf1d9535c63c4a39ee37a221bf10c80b2
nlwkobe30 <nlwkobe30@gmail.com>
2024-08-20 22:46:03 +0800

    update 684 union find

---
+++ 0684-redundant-connection/main.go
@@ -0,0 +1,53 @@
+package main
+
+func main() {
+
+}
+
+func findRedundantConnection(edges [][]int) []int {
+	n := len(edges)
+	parents := make([]int, 0, n)
+	ranks := make([]int, 0, n)
+	for i := range n {
+		parents = append(parents, i)
+		ranks = append(ranks, 1)
+	}
+
+	find := func(i int) int {
+		res := i
+		for res != parents[res] {
+			parents[res] = parents[parents[res]]
+			res = parents[res]
+		}
+		return res
+	}
+
+	union := func(i, j int) bool {
+		p1 := find(i)
+		p2 := find(j)
+
+		// same parent -> the same union set
+		if p1 == p2 {
+			return false
+		}
+
+		if ranks[p1] > ranks[p2] {
+			parents[p2] = p1
+			ranks[p1] += ranks[p2]
+		} else {
+			parents[p1] = p2
+			ranks[p2] += ranks[p1]
+		}
+
+		return true
+	}
+
+	for _, e := range edges {
+		// offset -1 because vertices value starts with 1
+		if !union(e[0]-1, e[1]-1) {
+			return e
+		}
+	}
+
+	return nil
+}
