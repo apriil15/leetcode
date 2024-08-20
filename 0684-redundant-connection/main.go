6be06bcaf0ece576da94b48e3b063a139a444b86
nlwkobe30 <nlwkobe30@gmail.com>
2024-08-20 23:17:17 +0800

    update 684 union find without -1 offset

--- 0684-redundant-connection/main.go
+++ 0684-redundant-connection/main.go
@@ -51,3 +51,50 @@ func findRedundantConnection(edges [][]int) []int {
 
 	return nil
 }
+
+func findRedundantConnection_without_offset(edges [][]int) []int {
+	n := len(edges)
+	parents := []int{-1}
+	ranks := []int{-1}
+	for i := 1; i <= n; i++ {
+		parents = append(parents, i)
+		ranks = append(ranks, 1)
+	}
+
+	find := func(i int) int {
+		res := i
+		for res != parents[res] {
+			parents[res] = parents[parents[res]]
+			res = parents[res]
+		}
+		return res
+	}
+
+	union := func(i, j int) bool {
+		p1 := find(i)
+		p2 := find(j)
+
+		// same parent -> the same union set
+		if p1 == p2 {
+			return false
+		}
+
+		if ranks[p1] > ranks[p2] {
+			parents[p2] = p1
+			ranks[p1] += ranks[p2]
+		} else {
+			parents[p1] = p2
+			ranks[p2] += ranks[p1]
+		}
+
+		return true
+	}
+
+	for _, e := range edges {
+		if !union(e[0], e[1]) {
+			return e
+		}
+	}
+
+	return nil
+}
