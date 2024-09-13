75712c67eef96488b443c4e9fa03b957d96274b8
nlwkobe30 <nlwkobe30@gmail.com>
2024-09-13 22:07:04 +0800

    update 217

--- 0217-contains-duplicate/main.go
+++ 0217-contains-duplicate/main.go
@@ -4,19 +4,16 @@ func main() {
 
 }
 
-// https://leetcode.com/problems/contains-duplicate/
 // time: O(n)
 // space: O(n)
 func containsDuplicate(nums []int) bool {
-	set := make(map[int]struct{})
+	seen := make(map[int]struct{})
 
-	for _, num := range nums {
-		if _, ok := set[num]; ok {
+	for _, n := range nums {
+		if _, ok := seen[n]; ok {
 			return true
 		}
-
-		set[num] = struct{}{}
+		seen[n] = struct{}{}
 	}
-
 	return false
 }
