81aa56cfa4c1399f417444b831751b7023631d0d
nlwkobe30 <nlwkobe30@gmail.com>
2024-10-07 23:59:38 +0800

    update 20

--- 0020-Valid-Parentheses/main.go
+++ 0020-Valid-Parentheses/main.go
@@ -7,31 +7,32 @@ func main() {
 // time: O(n)
 // space: O(n)
 func isValid(s string) bool {
-	stack := make([]byte, 0)
-
-	closeToOpen := map[byte]byte{
-		')': '(',
-		']': '[',
-		'}': '{',
+	var stack []byte
+	openToClose := map[byte]byte{
+		'(': ')',
+		'[': ']',
+		'{': '}',
 	}
 
 	for i := 0; i < len(s); i++ {
-		b := s[i]
-		if isOpenBracket(b) {
-			stack = append(stack, b)
-			continue
-		}
-
-		if len(stack) > 0 && stack[len(stack)-1] == closeToOpen[b] {
-			stack = stack[:len(stack)-1] // pop last one
+		if isOpen(s[i]) {
+			stack = append(stack, s[i])
 		} else {
-			return false
+			if len(stack) == 0 {
+				return false
+			}
+
+			open := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			if openToClose[open] != s[i] {
+				return false
+			}
 		}
 	}
-
 	return len(stack) == 0
 }
 
-func isOpenBracket(b byte) bool {
+func isOpen(b byte) bool {
 	return b == '(' || b == '[' || b == '{'
 }
