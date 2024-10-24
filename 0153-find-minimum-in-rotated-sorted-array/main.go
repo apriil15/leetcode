6145723a9218e761e87183b3607cca84dab4d189
nlwkobe30 <nlwkobe30@gmail.com>
2024-10-25 01:16:02 +0800

    update 153

---
+++ 0153-find-minimum-in-rotated-sorted-array/main.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+
+}
+
+// time: O(log n)
+// space: O(1)
+func findMin(nums []int) int {
+	res := nums[0]
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		if nums[left] < nums[right] {
+			return min(res, nums[left])
+		}
+
+		mid := left + (right-left)/2
+		res = min(res, nums[mid])
+
+		if nums[mid] >= nums[left] {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return res
+}
