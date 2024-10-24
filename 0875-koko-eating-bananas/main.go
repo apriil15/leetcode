9ec80360d71c2aab0af48a1cb17fa9dc41cbd0bd
nlwkobe30 <nlwkobe30@gmail.com>
2024-10-24 23:10:57 +0800

    update 875

--- 0875-koko-eating-bananas/main.go
+++ 0875-koko-eating-bananas/main.go
@@ -6,7 +6,7 @@ func main() {
 
 }
 
-// time: O(n log m) (m: maxPile, n: len(piles))
+// time: O(n log m) (n: len(piles), m: maxPile)
 // space: O(1)
 func minEatingSpeed(piles []int, h int) int {
 	maxPile := piles[0]
@@ -23,16 +23,16 @@ func minEatingSpeed(piles []int, h int) int {
 	for left <= right {
 		mid := left + (right-left)/2
 
-		var totalCostTime int
+		var totalTime int
 		for _, p := range piles {
-			totalCostTime += int(math.Ceil(float64(p) / float64(mid)))
+			totalTime += int(math.Ceil(float64(p) / float64(mid)))
 		}
 
-		if totalCostTime > h {
-			left = mid + 1
-		} else {
+		if totalTime <= h {
 			right = mid - 1
 			res = min(res, mid)
+		} else {
+			left = mid + 1
 		}
 	}
 	return res
