f105059d02b25f19f29de301fcfc37a65a77ee6d
nlwkobe30 <nlwkobe30@gmail.com>
2024-10-28 23:25:35 +0800

    update 33

--- 0033-search-in-rotated-sorted-array/main.go
+++ 0033-search-in-rotated-sorted-array/main.go
@@ -11,32 +11,28 @@ func main() {
 func search(nums []int, target int) int {
 	left := 0
 	right := len(nums) - 1
-
 	for left <= right {
 		mid := left + (right-left)/2
-
 		if nums[mid] == target {
 			return mid
 		}
 
 		// left side is ordered
 		if nums[left] <= nums[mid] {
-			// target is in this side
-			if nums[left] <= target && target < nums[mid] {
+			// target is here
+			if nums[left] <= target && target <= nums[mid] {
 				right = mid - 1
 			} else {
 				left = mid + 1
 			}
 		} else {
-			// right side is ordered
-			// and target is in this side
-			if nums[mid] < target && target <= nums[right] {
+			// right side is ordered and target is here
+			if nums[mid] <= target && target <= nums[right] {
 				left = mid + 1
 			} else {
 				right = mid - 1
 			}
 		}
 	}
-
 	return -1
 }
